2605d8f73f65f3ae9234b0daa8586ef73e0e094f
dgoscn <diego-ii@live.com>
2019-08-18 23:09:38 -0300

    setting routes variable

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :events, only: [:create]
+  resources :issues, only: [:index, :show] do
+    resources :events, only: [:index]
+  end
 end
