9683b6c27acf4ba0b84c7a023a0a4b64c2679c8c
dgoscn <diego-ii@live.com>
2019-08-19 00:15:45 -0300

    trying routes with gemfile wbhooks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :issues, only: [:index, :show] do
     resources :events, only: [:index]
   end
+  #resource :github_webhooks, only: :create, defaults: { formats: :json }
 end
