12ef3c811729db44da3317191a504393e75fb2d2
dgoscn <diego-ii@live.com>
2019-08-19 00:16:20 -0300

    trying title to issues

--- db/migrate/20190818185346_create_issues.rb
+++ db/migrate/20190818185346_create_issues.rb
@@ -2,6 +2,7 @@ class CreateIssues < ActiveRecord::Migration[6.0]
   def change
     create_table :issues do |t|
       t.integer :number
+      t.string :title
       t.string :github_id
 
       t.timestamps
